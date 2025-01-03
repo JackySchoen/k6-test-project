7599fb8104eb983d492c26c2829ce370dcc59e6b
Jacky Schoen <jschoen@hotmail.nl>
2025-01-03 15:39:52 +0100

    server.ts code cleanup

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,16 @@
-import express from 'express';
+import express from "express";
+import fs from "fs";
 
 const app = express();
 const port = 3000;
+const testSummaryDir = "test/summary";
 
-app.get('/', (_req, res) => {
-  res.send('Hello, this is my k6 test project!');
+if (!fs.existsSync(testSummaryDir)) fs.mkdirSync(testSummaryDir);
+
+app.get("/", (_req, res) => {
+    res.send("hi, this is my k6 test project! :)");
 });
 
 app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+    console.log(`server is running on http://localhost:${port}`);
 });
